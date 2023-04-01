62752d5851422a0cb8a1e8e1b2edead0a31752f2
root <root@ip-172-31-15-44.us-east-2.compute.internal>
2023-04-01 07:56:15 +0000

    added terrform files

---
+++ terraform_files/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+}
+
+resource "aws_key_pair" "ansible_key" {
+  key_name   = "ansible_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "ansible_key.pem"
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = aws_key_pair.ansible_key.key_name
+  security_groups = ["${aws_security_group.allow_tls.name}"]
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "ansible_sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ansible_sg"
+  }
+}
---
+++ terraform_files/var.tfvars
@@ -0,0 +1,3 @@
+ami="ami-0984b842ee24d45f1"
+instance_type="t2.micro"
+instance_name="ansible_worker"
